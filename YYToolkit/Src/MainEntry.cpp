8f1f09400beea36034ec41f5bc49f3abfe77c228
Tomat <xxlennygamerxx@gmail.com>
2023-11-12 15:47:55 -0800

    Oops.

--- YYToolkit/Src/MainEntry.cpp
+++ YYToolkit/Src/MainEntry.cpp
@@ -51,7 +51,7 @@ void __stdcall Main(HINSTANCE g_hDLL)
 
 	if (!steamApi)
 	{
-		Utils::Logging::Message(CLR_GRAY, "Failed to load Steam API, assuming not present.");
+		Utils::Logging::Message(CLR_GRAY, "Failed to load Steam API, assuming not present or non-Steam game.");
 	}
 	else
 	{
